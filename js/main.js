d02e0bcb2fae46af154857bf25c04454f9564249
AntonRudko <anton_rudko@knu.ua>
2025-08-28 13:50:23 +0300

    swioer sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-const menuBtn = document.querySelector('.menu__btn')
-const menuMobile = document.querySelector('.header__menu-list')
+const menuBtn = document.querySelector('.menu__btn');
+const menuMobile = document.querySelector('.header__menu-list');
 
 menuBtn.addEventListener('click', () => {
-	menuMobile.classList.toggle('menu--open')
-})
+	menuMobile.classList.toggle('menu--open');
+});
+
+const swiperOne = new Swiper('.feedback__slider', {
+	navigation: { nextEl: '.swiper-button-right' },
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+	// navigation: { nextEl: '.swiper-button-right' },
+	loop: true,
+	slidesPerView: 3,
+	spaceBetween: 20,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 3,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		360: {
+			slidesPerView: 1,
+		},
+	},
+});
